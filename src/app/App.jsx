f969bcfc4a85fec0f608ed7fc9df5aa010d57d5d
Shiroyam-token <semenmaslina@gmail.com>
2022-12-08 15:22:00 +0300

    refactor: rename on header

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
-import { Folder } from "../shared/ui/folder";
-import { Mail } from "../shared/ui/icon";
-import { Nav } from "../widgets/nav";
+import { Header } from "../widgets/header";
 import "./styles/index.css";
 
 function App() {
@@ -13,7 +11,7 @@ function App() {
     document.body.setAttribute("data-theme", theme);
   }, [theme]);
 
-  return <Folder svg={<Mail />} text="Входящие" />;
+  return <Header />;
 }
 
 export default App;
--- src/widgets/header/index.jsx
+++ src/widgets/header/index.jsx
@@ -2,11 +2,11 @@ import styles from "./index.module.css";
 import { logoText } from "../../shared/ui/icon";
 import { logo } from "../../shared/ui/icon";
 
-export const Nav = () => {
+export const Header = () => {
   return (
-    <nav className={styles.nav}>
+    <header className={styles.header}>
       <img className={styles.logoText} src={logoText} alt="logo" />
       <img className={styles.logo} src={logo} alt="logo" />
-    </nav>
+    </header>
   );
 };
