8f3f6732f77db0f29592655c7465a077bb85850a
Semyon Zasov <semenmaslina@gmail.com>
2022-12-18 22:03:21 +0300

    feat(lib): add portal

---
+++ src/shared/lib/portal.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+
+export const Portal = ({ children }) => {
+  const [container] = useState(() => document.createElement("div"));
+
+  useEffect(() => {
+    document.body.appendChild(container);
+
+    return () => {
+      document.body.removeChild(container);
+    };
+  }, []);
+
+  return createPortal(children, container);
+};
